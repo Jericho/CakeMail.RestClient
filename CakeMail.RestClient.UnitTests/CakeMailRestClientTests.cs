f101b17e74ade05ff237554c6e89abd9b8ec6058
Jeremie Desautels <desautelsj@hotmail.com>
2015-03-11 17:07:44 -0500

    Fix unit test project path Add files in the unit testing project

---
+++ CakeMail.RestClient.UnitTests/CakeMailRestClientTests.cs
@@ -0,0 +1,167 @@
+﻿using CakeMail.RestClient;
+using CakeMail.RestClient.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RestSharp;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace CakeMail.RestCLient.UnitTests
+{
+	[TestClass]
+	public class CakeMailRestClientTests
+	{
+		private const string API_KEY = "...dummy API key...";
+
+		[TestMethod]
+		[ExpectedException(typeof(HttpException))]
+		public void RestClient_Throws_exception_when_responsestatus_is_error()
+		{
+			// Arrange
+			var mockRestClient = new Mock<IRestClient>(MockBehavior.Strict);
+			mockRestClient.Setup(m => m.BaseUrl).Returns(new Uri("http://localhost"));
+			mockRestClient.Setup(m => m.Execute(It.Is<IRestRequest>(r =>
+				r.Parameters.Any(p => p.Name == "apikey" && p.Value.ToString() == API_KEY) &&
+				r.Parameters.Count(p => p.Type == ParameterType.GetOrPost) == 0
+			))).Returns(new RestResponse()
+			{
+				ResponseStatus = RestSharp.ResponseStatus.Error
+			});
+
+			// Act
+			var apiClient = new CakeMailRestClient(API_KEY, mockRestClient.Object);
+			var result = apiClient.GetCountries();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(HttpException))]
+		public void RestClient_Throws_exception_when_request_is_successful_but_response_content_is_empty()
+		{
+			// Arrange
+			var mockRestClient = new Mock<IRestClient>(MockBehavior.Strict);
+			mockRestClient.Setup(m => m.BaseUrl).Returns(new Uri("http://localhost"));
+			mockRestClient.Setup(m => m.Execute(It.Is<IRestRequest>(r =>
+				r.Parameters.Any(p => p.Name == "apikey" && p.Value.ToString() == API_KEY) &&
+				r.Parameters.Count(p => p.Type == ParameterType.GetOrPost) == 0
+			))).Returns(new RestResponse()
+			{
+				ResponseStatus = RestSharp.ResponseStatus.Completed,
+				StatusCode = HttpStatusCode.OK,
+				Content = ""
+			});
+
+			// Act
+			var apiClient = new CakeMailRestClient(API_KEY, mockRestClient.Object);
+			var result = apiClient.GetCountries();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(CakeMailException))]
+		public void RestClient_Throws_exception_when_request_is_successful_but_response_contenttype_is_not_specified()
+		{
+			// Arrange
+			var mockRestClient = new Mock<IRestClient>(MockBehavior.Strict);
+			mockRestClient.Setup(m => m.BaseUrl).Returns(new Uri("http://localhost"));
+			mockRestClient.Setup(m => m.Execute(It.Is<IRestRequest>(r =>
+				r.Parameters.Any(p => p.Name == "apikey" && p.Value.ToString() == API_KEY) &&
+				r.Parameters.Count(p => p.Type == ParameterType.GetOrPost) == 0
+			))).Returns(new RestResponse()
+			{
+				ResponseStatus = RestSharp.ResponseStatus.Completed,
+				StatusCode = HttpStatusCode.OK,
+				Content = "dummy content"
+			});
+
+			// Act
+			var apiClient = new CakeMailRestClient(API_KEY, mockRestClient.Object);
+			var result = apiClient.GetCountries();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(HttpException))]
+		public void RestClient_Throws_exception_when_responsescode_is_between_400_and_499_and_content_is_empty()
+		{
+			// Arrange
+			var mockRestClient = new Mock<IRestClient>(MockBehavior.Strict);
+			mockRestClient.Setup(m => m.BaseUrl).Returns(new Uri("http://localhost"));
+			mockRestClient.Setup(m => m.Execute(It.Is<IRestRequest>(r =>
+				r.Parameters.Any(p => p.Name == "apikey" && p.Value.ToString() == API_KEY) &&
+				r.Parameters.Count(p => p.Type == ParameterType.GetOrPost) == 0
+			))).Returns(new RestResponse()
+			{
+				ResponseStatus = RestSharp.ResponseStatus.Completed,
+				StatusCode = HttpStatusCode.Forbidden,
+				Content = ""
+			});
+
+			// Act
+			var apiClient = new CakeMailRestClient(API_KEY, mockRestClient.Object);
+			var result = apiClient.GetCountries();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(HttpException))]
+		public void RestClient_Throws_exception_when_responsescode_is_between_400_and_499_and_content_is_not_empty()
+		{
+			// Arrange
+			var mockRestClient = new Mock<IRestClient>(MockBehavior.Strict);
+			mockRestClient.Setup(m => m.BaseUrl).Returns(new Uri("http://localhost"));
+			mockRestClient.Setup(m => m.Execute(It.Is<IRestRequest>(r =>
+				r.Parameters.Any(p => p.Name == "apikey" && p.Value.ToString() == API_KEY) &&
+				r.Parameters.Count(p => p.Type == ParameterType.GetOrPost) == 0
+			))).Returns(new RestResponse()
+			{
+				ResponseStatus = RestSharp.ResponseStatus.Completed,
+				StatusCode = HttpStatusCode.Forbidden,
+				Content = "dummy content"
+			});
+
+			// Act
+			var apiClient = new CakeMailRestClient(API_KEY, mockRestClient.Object);
+			var result = apiClient.GetCountries();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(HttpException))]
+		public void RestClient_Throws_exception_when_responsescode_is_between_500_and_599()
+		{
+			// Arrange
+			var mockRestClient = new Mock<IRestClient>(MockBehavior.Strict);
+			mockRestClient.Setup(m => m.BaseUrl).Returns(new Uri("http://localhost"));
+			mockRestClient.Setup(m => m.Execute(It.Is<IRestRequest>(r =>
+				r.Parameters.Any(p => p.Name == "apikey" && p.Value.ToString() == API_KEY) &&
+				r.Parameters.Count(p => p.Type == ParameterType.GetOrPost) == 0
+			))).Returns(new RestResponse()
+			{
+				ResponseStatus = RestSharp.ResponseStatus.Completed,
+				StatusCode = HttpStatusCode.InternalServerError
+			});
+
+			// Act
+			var apiClient = new CakeMailRestClient(API_KEY, mockRestClient.Object);
+			var result = apiClient.GetCountries();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(HttpException))]
+		public void RestClient_Throws_exception_when_responsescode_is_greater_than_599()
+		{
+			// Arrange
+			var mockRestClient = new Mock<IRestClient>(MockBehavior.Strict);
+			mockRestClient.Setup(m => m.BaseUrl).Returns(new Uri("http://localhost"));
+			mockRestClient.Setup(m => m.Execute(It.Is<IRestRequest>(r =>
+				r.Parameters.Any(p => p.Name == "apikey" && p.Value.ToString() == API_KEY) &&
+				r.Parameters.Count(p => p.Type == ParameterType.GetOrPost) == 0
+			))).Returns(new RestResponse()
+			{
+				ResponseStatus = RestSharp.ResponseStatus.Completed,
+				StatusCode = (HttpStatusCode)600
+			});
+
+			// Act
+			var apiClient = new CakeMailRestClient(API_KEY, mockRestClient.Object);
+			var result = apiClient.GetCountries();
+		}
+	}
+}
---
+++ CakeMail.RestClient.UnitTests/Utilities/CakeMailDateTimeConverter.cs
@@ -0,0 +1,67 @@
+﻿using CakeMail.RestClient.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.IO;
+
+namespace CakeMail.RestCLient.UnitTests.Utilities
+{
+	[TestClass]
+	public class CakeMailDateTimeConverterTests
+	{
+		[TestMethod]
+		public void Successfully_parses_json()
+		{
+			// Arrange
+			var json = "\"2015-03-11 15:21:00\"";
+
+			var converter = new CakeMailDateTimeConverter();
+			var reader = new JsonTextReader(new StringReader(json));
+			reader.Read();
+
+			// Act
+			var result = (DateTime)converter.ReadJson(reader, typeof(DateTime), null, null);
+
+			// Assert
+			Assert.AreEqual(2015, result.Year);
+			Assert.AreEqual(3, result.Month);
+			Assert.AreEqual(11, result.Day);
+			Assert.AreEqual(15, result.Hour);
+			Assert.AreEqual(21, result.Minute);
+			Assert.AreEqual(0, result.Second);
+		}
+
+		[TestMethod]
+		public void Successfully_parses_empty_date()
+		{
+			// Arrange
+			var json = "\"0000-00-00 00:00:00\"";
+
+			var converter = new CakeMailDateTimeConverter();
+			var reader = new JsonTextReader(new StringReader(json));
+			reader.Read();
+
+			// Act
+			var result = (DateTime)converter.ReadJson(reader, typeof(DateTime), null, null);
+
+			// Assert
+			Assert.AreEqual(DateTime.MinValue, result);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(Exception))]
+		public void Throws_exception_when_content_is_not_a_string()
+		{
+			// Arrange
+			var json = "1234";
+
+			var converter = new CakeMailDateTimeConverter();
+			var reader = new JsonTextReader(new StringReader(json));
+			reader.Read();
+
+			// Act
+			var result = (DateTime)converter.ReadJson(reader, typeof(DateTime), null, null);
+		}
+	}
+}
