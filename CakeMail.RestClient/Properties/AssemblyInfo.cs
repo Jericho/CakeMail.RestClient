e72cc9aa08a1079afe2d7987e5257692f73ca4c1
Jeremie Desautels <desautelsj@hotmail.com>
2015-03-16 12:14:22 -0500

    Fix invalid nuspec Remove version info from AssemblyInfo.cs to clearly illustrate that AppVeyor will patch the file Specify the build platform and configuration in the yaml file

--- CakeMail.RestClient/Properties/AssemblyInfo.cs
+++ CakeMail.RestClient/Properties/AssemblyInfo.cs
@@ -11,17 +11,3 @@
 [assembly: AssemblyCopyright("Copyright Jeremie Desautels © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha001")]
