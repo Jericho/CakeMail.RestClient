88bc57488717aef81f6ecb8a12403beeb3b980cf
Jeremie Desautels <desautelsj@hotmail.com>
2015-03-25 10:31:58 -0500

    Add missing properties on the Client model

--- CakeMail.RestClient/Models/Client.cs
+++ CakeMail.RestClient/Models/Client.cs
@@ -11,11 +11,41 @@ namespace CakeMail.RestClient.Models
 		[JsonProperty("id")]
 		public int Id { get; set; }
 
+		[JsonProperty("lineage")]
+		public string Lineage { get; set; }
+
+		[JsonProperty("address1")]
+		public string Address1 { get; set; }
+
+		[JsonProperty("address2")]
+		public string Address2 { get; set; }
+
+		[JsonProperty("config_id")]
+		public string ConfigId { get; set; }
+
+		[JsonProperty("auth_domain")]
+		public string AuthDomain { get; set; }
+
+		[JsonProperty("bounce_domain")]
+		public string BounceDomain { get; set; }
+
+		[JsonProperty("parent_bounce_domain")]
+		public string ParentBounceDomain { get; set; }
+
+		[JsonProperty("city")]
+		public string City { get; set; }
+
 		[JsonProperty("company_name")]
 		public string Name { get; set; }
 
 		[JsonProperty("contact_id")]
 		public int ContactId { get; set; }
+
+		[JsonProperty("country")]
+		public string CountryName { get; set; }
+
+		[JsonProperty("country_id")]
+		public string CountryId { get; set; }
 
 		[JsonProperty("currency")]
 		public string Currency { get; set; }
@@ -26,51 +56,129 @@ namespace CakeMail.RestClient.Models
 		[JsonProperty("default_month_limit")]
 		public int DefaultMonthLimit { get; set; }
 
-		[JsonProperty("default_contact_limit")]
-		public int DefaultContactLimit { get; set; }
+		[JsonProperty("dkim_domain")]
+		public string DkimDomain { get; set; }
 
-		[JsonProperty("reseller")]
-		public bool IsReseller { get; set; }
+		[JsonProperty("doptin_ip")]
+		public string DoptinIp { get; set; }
+
+		[JsonProperty("fax")]
+		public string Fax { get; set; }
+
+		[JsonProperty("force_unsub")]
+		public bool ForceUnsubscribe { get; set; }
+
+		[JsonProperty("forward_domain")]
+		public string ForwardDomain { get; set; }
+
+		[JsonProperty("parent_forward_domain")]
+		public string ParentForwardDomain { get; set; }
+
+		[JsonProperty("forward_ip")]
+		public string ForwardIp { get; set; }
+
+		[JsonProperty("key")]
+		public string Key { get; set; }
+
+		[JsonProperty("last_activity")]
+		public DateTime LastActivityOn { get; set; }
 
 		[JsonProperty("mailing_limit")]
 		public int MailingLimit { get; set; }
 
+		[JsonProperty("manager_id")]
+		public int ManagerId { get; set; }
+
+		[JsonProperty("md_domain")]
+		public string MdDomain { get; set; }
+
+		[JsonProperty("parent_md_domain")]
+		public string ParentMdDomain { get; set; }
+
 		[JsonProperty("month_limit")]
 		public int MonthLimit { get; set; }
+
+		[JsonProperty("default_contact_limit")]
+		public int DefaultContactLimit { get; set; }
 
 		[JsonProperty("contact_limit")]
 		public int ContactLimit { get; set; }
 
-		[JsonProperty("manager_id")]
-		public int ManagerId { get; set; }
+		[JsonProperty("mta_id")]
+		public string MtaId { get; set; }
 
 		[JsonProperty("parent_id")]
 		public int ParentId { get; set; }
+
+		[JsonProperty("phone")]
+		public string Phone { get; set; }
+
+		[JsonProperty("plan_min_vol")]
+		public string PlanMinVol { get; set; }
+
+		[JsonProperty("plan_period")]
+		public string PlanPeriod { get; set; }
+
+		[JsonProperty("plan_price")]
+		public string PlanPrice { get; set; }
+
+		[JsonProperty("plan_start_date")]
+		public string PlanStartDate { get; set; }
+
+		[JsonProperty("plan_type")]
+		public string PlanType { get; set; }
+
+		[JsonProperty("postal_code")]
+		public string PostalCode { get; set; }
+
+		[JsonProperty("pricing_plan")]
+		public string PricingPlan { get; set; }
+
+		[JsonProperty("province")]
+		public string ProvinceName { get; set; }
+
+		[JsonProperty("province_id")]
+		public string ProvinceId { get; set; }
 
 		[JsonProperty("registered_date")]
 		[JsonConverter(typeof(CakeMailDateTimeConverter))]
 		public DateTime RegisteredOn { get; set; }
 
+		[JsonProperty("reseller")]
+		public bool IsReseller { get; set; }
+
 		[JsonProperty("status")]
 		public string Status { get; set; }
 
-		[JsonProperty("md_domain")]
-		public string MdDomain { get; set; }
+		[JsonProperty("lineage_status")]
+		public string LineageStatus { get; set; }
 
-		[JsonProperty("bounce_domain")]
-		public string BounceDomain { get; set; }
+		[JsonProperty("default_trial")]
+		public string DefaultTrial { get; set; }
 
-		[JsonProperty("forward_domain")]
-		public string ForwardDomain { get; set; }
+		[JsonProperty("default_reseller")]
+		public string DefaultReseller { get; set; }
 
-		[JsonProperty("office_phone")]
-		public string OfficePhone { get; set; }
+		[JsonProperty("billing_bundle_id")]
+		public string BillingBundleId { get; set; }
 
-		[JsonProperty("mobile_phone")]
-		public string MobilePhone { get; set; }
+		[JsonProperty("billing_package_id")]
+		public string BillingPackageId { get; set; }
 
-		[JsonProperty("lineage")]
-		public string lineage { get; set; }
+		[JsonProperty("billing_discount_percent")]
+		public string BillingDiscountPercent { get; set; }
+
+		[JsonProperty("callback")]
+		public string Callback { get; set; }
+
+		[JsonProperty("trigger_info")]
+		public string TriggerInfo { get; set; }
+
+		[JsonProperty("website")]
+		public string Website { get; set; }
+
+		[JsonProperty("industry_id")]
+		public string IndustryId { get; set; }
 
 		#endregion
 	}
