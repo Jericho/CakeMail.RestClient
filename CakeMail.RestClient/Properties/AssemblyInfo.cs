a90f9074540d20f7e937211f621a9d59022d5774
Jeremie Desautels <desautelsj@hotmail.com>
2015-04-10 16:21:47 -0500

    Increase nuget package version to 1.0.0-beta05

--- CakeMail.RestClient/Properties/AssemblyInfo.cs
+++ CakeMail.RestClient/Properties/AssemblyInfo.cs
@@ -24,4 +24,4 @@
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta04")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta05")]
