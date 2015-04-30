dae2d0fe225d1b3dfe67b40ae0d1a191390a3261
Jeremie Desautels <desautelsj@hotmail.com>
2015-04-30 10:54:33 -0500

    Add integration tests for Countries, Users and Clients

---
+++ CakeMail.RestClient.IntegrationTests/Countries.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace CakeMail.RestClient.IntegrationTests
+{
+	public static class CountriesTests
+	{
+		public static void ExecuteAllMethods(CakeMailRestClient api)
+		{
+			Console.WriteLine("");
+			Console.WriteLine(new string('-', 25));
+			Console.WriteLine("Executing COUNTRIES methods...");
+
+			var countries = api.GetCountries();
+			Console.WriteLine("Retrieved all countries. There are {0} countries.", countries.Count());
+
+			var canada = countries.Single(country => country.EnglishName == "Canada");
+			Console.WriteLine("Canada --> Id: {0}", canada.Id);
+
+			var canadianProvinces = api.GetProvinces(canada.Id);
+			Console.WriteLine("There are {0} canadian provinces/territories/etc.", canadianProvinces.Count());
+
+			var quebec = canadianProvinces.Single(province => province.EnglishName == "Quebec");
+			Console.WriteLine("Quebec --> Id: {0}", quebec.Id);
+
+			var usa = countries.Single(country => country.EnglishName == "United States");
+			Console.WriteLine("USA --> Id: {0}", usa.Id);
+
+			var americanStates = api.GetProvinces(usa.Id);
+			Console.WriteLine("There are {0} american states/territories/etc.", americanStates.Count());
+
+			var georgia = americanStates.Single(province => province.EnglishName == "Georgia");
+			Console.WriteLine("Georgia --> Id: {0}", georgia.Id);
+
+			var florida = americanStates.Single(province => province.EnglishName == "Florida");
+			Console.WriteLine("Florida --> Id: {0}", florida.Id);
+
+			Console.WriteLine(new string('-', 25));
+			Console.WriteLine("");
+		}
+	}
+}
---
+++ CakeMail.RestClient.IntegrationTests/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+
+namespace CakeMail.RestClient.IntegrationTests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("{0} Executing all CakeMail API methods ... {0}", new string('=', 10));
+
+			try
+			{
+				ExecuteAllMethods();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("An error has occured: {0}", e.Message);
+			}
+			finally
+			{
+				Console.WriteLine("");
+				Console.WriteLine("Press any key to close...");
+				Console.ReadKey();
+			}
+		}
+
+		static void ExecuteAllMethods()
+		{
+			var apiKey = ConfigurationManager.AppSettings["ApiKey"];
+			var userName = ConfigurationManager.AppSettings["UserName"];
+			var password = ConfigurationManager.AppSettings["Password"];
+			var overrideClientId = ConfigurationManager.AppSettings["OverrideClientId"];
+
+			var api = new CakeMailRestClient(apiKey);
+			var loginInfo = api.Login(userName, password);
+			var clientId = string.IsNullOrEmpty(overrideClientId) ? loginInfo.ClientId : long.Parse(overrideClientId);
+			var userKey = loginInfo.UserKey;
+
+			CountriesTests.ExecuteAllMethods(api);
+			ClientsTests.ExecuteAllMethods(api, userKey, clientId);
+			UsersTests.ExecuteAllMethods(api, userKey, clientId);
+		}
+	}
+}
