cd8edb7db83d96fe39daa293b3e0b95c80742e83
Jeremie Desautels <desautelsj@hotmail.com>
2016-11-19 16:56:37 -0600

    Standardize the AssemblyInfo

--- Source/CakeMail.RestClient/Properties/AssemblyInfo.cs
+++ Source/CakeMail.RestClient/Properties/AssemblyInfo.cs
@@ -1,27 +1,29 @@
 ﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CakeMail.RestClient")]
 [assembly: AssemblyDescription("CakeMail.RestClient is a .NET wrapper for the CakeMail API")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jeremie Desautels")]
 [assembly: AssemblyProduct("CakeMail.RestClient")]
 [assembly: AssemblyCopyright("Copyright Jeremie Desautels © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+// Make it easy to distinguish Debug and Release builds;
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("DDCD8C73-3E02-45CC-958F-1E0383D6EB91")]
+
