5d4b4991a00ac6b6aafe62639074dde4da44c36d
Jeremie Desautels <desautelsj@hotmail.com>
2017-06-20 14:12:19 -0400

    Better XML doc for the Timezone resource

--- Source/CakeMail.RestClient/Resources/ITimezones.cs
+++ Source/CakeMail.RestClient/Resources/ITimezones.cs
@@ -4,8 +4,16 @@ using System.Threading.Tasks;
 
 namespace CakeMail.RestClient.Resources
 {
+	/// <summary>
+	/// Allows you to manage timezones
+	/// </summary>
 	public interface ITimezones
 	{
+		/// <summary>
+		/// Retrieve the list of all timezones known to the CakeMail system
+		/// </summary>
+		/// <param name="cancellationToken">The cancellation token</param>
+		/// <returns>An enumeration of all <see cref="Timezone">timezones</see>.</returns>
 		Task<Timezone[]> GetAllAsync(CancellationToken cancellationToken = default(CancellationToken));
 	}
 }
