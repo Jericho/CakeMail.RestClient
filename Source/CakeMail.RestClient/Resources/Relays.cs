44fe69219dd9d05620cf8d26a60011e7da7af8b7
Jeremie Desautels <desautelsj@hotmail.com>
2017-06-20 13:50:23 -0400

    Better XML doc for relays

--- Source/CakeMail.RestClient/Resources/Relays.cs
+++ Source/CakeMail.RestClient/Resources/Relays.cs
@@ -8,6 +8,10 @@ using System.Threading.Tasks;
 
 namespace CakeMail.RestClient.Resources
 {
+	/// <summary>
+	/// Allows you to manage relays
+	/// </summary>
+	/// <seealso cref="CakeMail.RestClient.Resources.IRelays" />
 	public class Relays : IRelays
 	{
 		#region Fields
