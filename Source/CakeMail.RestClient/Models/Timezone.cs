0081fef643042d9aa4dad9a037cd0bbe1517172c
Jeremie Desautels <desautelsj@hotmail.com>
2017-06-22 11:42:07 -0400

    XML doc for Timezone

--- Source/CakeMail.RestClient/Models/Timezone.cs
+++ Source/CakeMail.RestClient/Models/Timezone.cs
@@ -2,13 +2,28 @@
 
 namespace CakeMail.RestClient.Models
 {
+	/// <summary>
+	/// A timezone
+	/// </summary>
 	public class Timezone
 	{
 		#region Properties
 
+		/// <summary>
+		/// Gets or sets the identifier.
+		/// </summary>
+		/// <value>
+		/// The identifier.
+		/// </value>
 		[JsonProperty("id")]
 		public long Id { get; set; }
 
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>
+		/// The name.
+		/// </value>
 		[JsonProperty("name")]
 		public string Name { get; set; }
 
