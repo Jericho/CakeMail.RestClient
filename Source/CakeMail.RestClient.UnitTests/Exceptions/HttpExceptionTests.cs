97160a67dd5b232d4e5901c7405d795b566cf6e8
Jeremie Desautels <desautelsj@hotmail.com>
2019-05-29 19:50:15 -0400

    Unit tests for exceptions with diagnostic log

--- Source/CakeMail.RestClient.UnitTests/Exceptions/HttpExceptionTests.cs
+++ Source/CakeMail.RestClient.UnitTests/Exceptions/HttpExceptionTests.cs
@@ -1,4 +1,4 @@
-﻿using CakeMail.RestClient.Exceptions;
+using CakeMail.RestClient.Exceptions;
 using Shouldly;
 using System;
 using System.Net;
@@ -15,14 +15,16 @@ namespace CakeMail.RestClient.UnitTests.Exceptions
 			var message = "This is a dummy message";
 			var httpStatus = HttpStatusCode.Ambiguous;
 			var uri = new Uri("http://unittesting.com", UriKind.Absolute);
+			var diagnosticLog = "This is the diagnostic log";
 
 			// Act
-			var exception = new HttpException(message, httpStatus, uri);
+			var exception = new HttpException(message, httpStatus, uri, diagnosticLog);
 
 			// Assert
 			exception.Message.ShouldBe(message);
 			exception.HttpStatus.ShouldBe(httpStatus);
 			exception.Uri.ShouldBe(uri);
+			exception.DiagnosticLog.ShouldBe(diagnosticLog);
 			exception.InnerException.ShouldBeNull();
 		}
 
@@ -33,15 +35,17 @@ namespace CakeMail.RestClient.UnitTests.Exceptions
 			var message = "This is a dummy message";
 			var httpStatus = HttpStatusCode.Ambiguous;
 			var uri = new Uri("http://unittesting.com", UriKind.Absolute);
+			var diagnosticLog = "This is the diagnostic log";
 			var innerException = new Exception("Testing");
 
 			// Act
-			var exception = new HttpException(message, httpStatus, uri, innerException);
+			var exception = new HttpException(message, httpStatus, uri, diagnosticLog, innerException);
 
 			// Assert
 			exception.Message.ShouldBe(message);
 			exception.HttpStatus.ShouldBe(httpStatus);
 			exception.Uri.ShouldBe(uri);
+			exception.DiagnosticLog.ShouldBe(diagnosticLog);
 			exception.InnerException.ShouldNotBeNull();
 			exception.InnerException.Message.ShouldBe(innerException.Message);
 		}
